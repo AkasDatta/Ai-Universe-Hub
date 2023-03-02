c863af451d4d2c3ba8158b08fd6a4d8d629cc0a0
Akas_Datta <012akasdatta@gmail.com>
2023-03-03 02:52:44 +0600

    fontawesome arrow icon, features details, see more button and stylesheet css created

--- js/style.js
+++ js/style.js
@@ -15,11 +15,15 @@ const displayData = tools =>{
         <img src="${tool.image}" class="card-img-top rounded" alt="...">
         <div class="card-body">
           <h5 class="card-title fw-bold">Features</h5>
-          <p class="card-text text-muted">1. ${tool.features[0]} </br>2. ${tool.features[1]} </br>3. ${tool.features[2]}</p>
+   
+          <p class="card-text text-muted">1. ${tool.features[0] ? tool.features[0]: ''} </br> 2. ${tool.features[1] ? tool.features[1]: ''} </br> 3. ${tool.features[2] ? tool.features[2]: ''} </br> ${tool.features[3] ? tool.features[3]: ''}</p> 
+
         </div>
         <hr>
-        <h5 class="card-title fw-bold m-3">${tool.name}</h5>
-        <p class="mx-3 text-muted">${tool.published_in}</p>
+        <h5 class="card-title fw-bold mx-3 mt-3">${tool.name}</h5>
+        <i class="fas fa-solid fa-arrow-right"></i>
+        <p class="mx-3 text-muted"><i class="fa-solid fa-calendar-days"></i> ${tool.published_in}
+        </p>
       </div>
         `;
         toolContainer.appendChild(toolDiv);
